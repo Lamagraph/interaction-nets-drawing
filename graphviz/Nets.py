f7cf17002d5010bac890a5c171820ce69ea0f0be
Vladimir Zaikin <friend.zva@yandex.ru>
2025-09-11 20:29:04 +0300

    feat: impl py script for generation DOT
    Signed-off-by: Vladimir Zaikin <friend.zva@yandex.ru>

---
+++ graphviz/Nets.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from typing import Set
+
+
+@dataclass
+class Port:
+    id: str
+    label: str | None
+    is_used: bool = False
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.id == other.id
+        return False
+
+    def __repr__(self):
+        return self.id
+
+
+@dataclass
+class Node:
+    id: str
+    label: str | None
+    auxiliary_ports: Set[Port]
+    principal_port: Port
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.id == other.id
+        return False
+
+    def __repr__(self):
+        return self.id
+
+
+@dataclass
+class Edge:
+    id: str
+    source: str
+    target: str
+    source_port: str
+    target_port: str
+    active_pair: bool | None
---
+++ graphviz/main.py
@@ -0,0 +1,176 @@
+from pathlib import Path
+import subprocess
+import json
+
+from typing import Any, Set
+
+from Nets import Port, Node, Edge
+
+ds = "  "
+tab = "    "
+
+
+def get_json(file: Path) -> Any:
+    with open(file, "r") as f:
+        return json.load(f)
+
+
+def gen_label_node(label: str, auxiliary_ports: Set[Port], principal_port: Port):
+    lines = '<TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0" CELLPADDING="0">\n'
+
+    if auxiliary_ports:
+        lines += ds + "<TR>\n"
+    for port in auxiliary_ports:
+        lines += ds + ds + f'<TD BORDER="0" PORT="{port}">{port.label or ""}</TD>\n'
+    if auxiliary_ports:
+        lines += ds + "</TR>\n"
+
+    if label:
+        lines += ds + f'<TR><TD BORDER="0" COLSPAN="2">{label or ""}</TD></TR>\n'
+
+    lines += (
+        ds
+        + f'<TR><TD BORDER="0" COLSPAN="2" PORT="{principal_port.id}">{principal_port.label  or ""}</TD></TR>\n'
+    )
+
+    return lines + "</TABLE>>\n"
+
+
+def gen_str_node(node_j, nodes: Set[Node]) -> str:
+    lines = ""
+
+    data = node_j["data"]
+    node = Node(
+        id=node_j["id"],
+        label=data["label"],
+        auxiliary_ports={Port(**port) for port in data["auxiliaryPorts"]},
+        principal_port=Port(**data["principalPort"]),
+    )
+
+    nodes.add(node)
+
+    line_fst = tab + f"{node} [\n"
+    line_snd = tab + tab + "label=<\n"
+    lines_label = gen_label_node(node.label, node.auxiliary_ports, node.principal_port)
+    line_last = tab + "];\n\n"
+
+    lines += line_fst + line_snd + lines_label + line_last
+
+    return lines
+
+
+def get_type_port(node, port) -> bool:
+    if node.principal_port.id == port:
+        node.principal_port.is_used = True
+        return True
+    else:
+        for p in node.auxiliary_ports:
+            if p.id == port:
+                p.is_used = True
+                break
+
+    return False
+
+
+def gen_str_edge(edge_j, nodes: Set[Node]) -> str:
+    line = ""
+
+    edge = Edge(
+        id=id,
+        source=edge_j["source"],
+        target=edge_j["target"],
+        source_port=edge_j["sourcePort"],
+        target_port=edge_j["targetPort"],
+        active_pair=edge_j["activePair"],
+    )
+
+    is_south_node_s, is_south_node_t = False, False
+    is_found_node_s, is_found_node_t = False, False
+    for node in nodes:
+        if not is_found_node_s and node.id == edge.source:
+            is_south_node_s = get_type_port(node, edge.source_port)
+            is_found_node_s = True
+
+        if not is_found_node_t and node.id == edge.target:
+            is_south_node_t = get_type_port(node, edge.target_port)
+            is_found_node_t = True
+
+    str_node_s = f"{edge.source}:{edge.source_port}:{'s' if is_south_node_s else 'n'}"
+    str_node_t = f"{edge.target}:{edge.target_port}:{'s' if is_south_node_t else 'n'}"
+
+    line = tab + str_node_s + " -- " + str_node_t
+
+    if edge.active_pair:
+        line += ' [constraint=false, color="blue", dir=both];\n'
+    elif is_south_node_s:
+        line += " [dir=back];\n"
+    elif is_south_node_t:
+        line += " [dir=forward];\n"
+    else:
+        line += ";\n"
+
+    return line
+
+
+def gen_str_edge_invis(port: Port, id_node: str, is_principal_port: bool) -> str:
+    lines = ""
+
+    if not port.is_used:
+        str_id_node = f"_N_for_free_port_{port}"
+        lines += tab + str_id_node + " [style=invis];\n"
+        lines += tab + "{ " + f"rank=same; {id_node}; {str_id_node}" + " };\n"
+        if is_principal_port:
+            lines += tab + f"{id_node}:{port}:s -- {str_id_node} [dir=back];\n"
+        else:
+            lines += tab + f"{id_node}:{port}:n -- {str_id_node};\n"
+
+    return lines
+
+
+def gen_str_graph(name_graph: str, data_json: Any):
+    lines = f"graph {name_graph}" + " {\n"
+    lines += (
+        tab
+        + 'node [shape=rect, style="filled, rounded", fillcolor=lightblue, margin=0, width=0.5, height=0.25];\n'
+    )
+    lines += tab + "edge [arrowtail=dot, arrowhead=dot, arrowsize=0.4];\n\n"
+
+    nodes: Set[Node] = set()
+    nodes_j = data_json["nodes"]
+    for node_j in nodes_j:
+        lines += gen_str_node(node_j, nodes)
+
+    edges_j = data_json["edges"]
+    for edge_j in edges_j:
+        lines += gen_str_edge(edge_j, nodes)
+    lines += "\n"
+
+    for node in nodes:
+        for port in node.auxiliary_ports:
+            lines += gen_str_edge_invis(port, node.id, False)
+        lines += gen_str_edge_invis(node.principal_port, node.id, True)
+
+    return lines + "}\n"
+
+
+def main(file: Path, need_save_dot: bool):
+    name_file = file.stem
+    data_json = get_json(file)
+
+    str_dot = gen_str_graph(name_file, data_json)
+
+    file_png = f"./saved-nets/{file.stem}.png"
+
+    if need_save_dot:
+        file_dot = f"./{file.stem}.dot"
+        with open(file_dot, "w") as f:
+            f.write(str_dot)
+
+        subprocess.run(["dot", "-Tpng", file_dot, "-o", file_png])
+    else:
+        subprocess.run(["dot", "-Tpng", "-o", file_png], input=str_dot, text=True)
+
+
+if __name__ == "__main__":
+    file = "../example-nets/app_list.json"
+    main(Path(file), False)
