11c56f3a1bb5012e2262dc32d55cc688fded80d9
Vladimir Zaikin <friend.zva@yandex.ru>
2025-09-11 14:36:55 +0300

    feat: toggle button for layouts
    Signed-off-by: Vladimir Zaikin <friend.zva@yandex.ru>

--- src/views/MenuLayouts.tsx
+++ src/views/MenuLayouts.tsx
@@ -1,4 +1,5 @@
 import { Panel, useNodesInitialized, useReactFlow } from '@xyflow/react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 import { type CustomNode } from '../nets';
 
@@ -16,6 +17,8 @@ export default () => {
   const [, { toggle }] = dForceLayoutNodes();
 
   const [isRunning, setIsRunning] = useState(false);
+
+  const [layoutsShowed, setLayoutsShowed] = useState(false);
 
   const dagreLayout = async (direction: string) => {
     if (nodesInitialized) {
@@ -57,26 +60,36 @@ export default () => {
 
   return (
     <Panel position='top-right' className='panel-layouts'>
-      <label className='xy-theme__label'>Layouts:</label>
       <div>
-        <button className='xy-theme__button' onClick={() => dagreLayout('TB')} disabled={isRunning} >
-          Dagre: vertical
+        {layoutsShowed && <label className='xy-theme__label'>Layouts</label>}
+      </div>
+
+      <div>
+        <button title={layoutsShowed ? 'Show less' : 'Show more'} className='xy-theme__button' onClick={() => setLayoutsShowed(!layoutsShowed)}>
+          {layoutsShowed ? <FaEyeSlash /> : <FaEye />}
         </button>
-        <button className='xy-theme__button' onClick={() => dagreLayout('LR')} disabled={isRunning} >
-          Dagre: horizontal
-        </button>
-        <button className='xy-theme__button' onClick={elkHandlesLayout} disabled={isRunning} >
-          ELK-handles
-        </button>
-        <button className='xy-theme__button' onClick={() => elkLayout('DOWN')} disabled={isRunning} >
-          ELK: vertical
-        </button>
-        <button className='xy-theme__button' onClick={() => elkLayout('RIGHT')} disabled={isRunning} >
-          ELK: horizontal
-        </button>
-        <button className='xy-theme__button' onClick={dLayout} disabled={isRunning} >
-          D3-hierarchy
-        </button>
+        {layoutsShowed && (
+          <>
+            <button className='xy-theme__button' onClick={() => dagreLayout('TB')} disabled={isRunning} >
+              Dagre: vertical
+            </button>
+            <button className='xy-theme__button' onClick={() => dagreLayout('LR')} disabled={isRunning} >
+              Dagre: horizontal
+            </button>
+            <button className='xy-theme__button' onClick={elkHandlesLayout} disabled={isRunning} >
+              ELK-handles
+            </button>
+            <button className='xy-theme__button' onClick={() => elkLayout('DOWN')} disabled={isRunning} >
+              ELK: vertical
+            </button>
+            <button className='xy-theme__button' onClick={() => elkLayout('RIGHT')} disabled={isRunning} >
+              ELK: horizontal
+            </button>
+            <button className='xy-theme__button' onClick={dLayout} disabled={isRunning} >
+              D3-hierarchy
+            </button>
+          </>
+        )}
         <button className='xy-theme__button' onClick={dForceLayout}>
           {isRunning ? 'Stop' : 'Start'} D3-force
         </button>
