146e4fd9dadbbcbaf6c766671ffd3d48f762c464
Vladimir Zaikin <friend.zva@yandex.ru>
2025-09-18 19:17:03 +0300

    fix: upload net with some error
    Signed-off-by: Vladimir Zaikin <friend.zva@yandex.ru>

--- react-flow/src/nets.ts
+++ react-flow/src/nets.ts
@@ -79,60 +79,66 @@ export async function parseJSON(
     },
     typeEdge: string,
 ): Promise<[Agent[], Edge[]]> {
-    const nodesObj = net.nodes ? Object.entries(net.nodes) : [];
-    const nodes: Agent[] = [];
-    let index = 0;
-    nodesObj.forEach(([, node]) => {
-        if (
-            validate(node.id) &&
-            (validate(node.data?.label) || validate(node.label)) &&
-            (Array.isArray(node.data?.auxiliaryPorts) || Array.isArray(node.auxiliaryPorts)) &&
-            (validate(node.data?.principalPort.id) || validate(node.principalPort.id))
-        ) {
-            nodes.push({
-                id: node.id,
-                data: node.data ?? {
-                    label: node.label,
-                    auxiliaryPorts: node.auxiliaryPorts,
-                    principalPort: node.principalPort
-                },
-                position: node.position || {
-                    x: 50 + 300 * Math.floor(index / 5),
-                    y: 50 + 120 * (index % 5)
-                },
-                type: 'agent',
-                // style: { transform: 'rotate(90deg)', transformOrigin: 'center center' }
-            });
-            index += 1;
-        } else {
-            console.warn(`Invalid node data structure for id: ${node.id}`);
-        }
-    });
+    try {
+        const nodesObj = net.nodes ? Object.entries(net.nodes) : [];
+        const nodes: Agent[] = [];
+        let index = 0;
 
-    const edgesObj = net.edges ? Object.entries(net.edges) : [];
-    const edges: Edge[] = []
-    edgesObj.forEach(([, edge]) => {
-        if (
-            validate(edge.source) && validate(edge.target) &&
-            (validate(edge.sourcePort) || validate(edge.sourceHandle)) &&
-            (validate(edge.targetPort) || validate(edge.targetHandle))
-        ) {
-            const sourceHandle = edge.sourcePort || edge.sourceHandle;
-            const targetHandle = edge.targetPort || edge.targetHandle;
-            edges.push({
-                id: edge.id || `E_${edge.source}:${sourceHandle}-${edge.target}:${targetHandle}`,
-                source: edge.source,
-                target: edge.target,
-                sourceHandle: sourceHandle,
-                targetHandle: `${targetHandle}t`,
-                animated: edge.activePair ?? edge.animated ?? false,
-                style: (edge.activePair || edge.animated) ? { stroke: 'blue' } : {},
-                type: typeEdge,
-            });
-        } else {
-            console.warn(`Invalid edge data structure for id: ${edge.id}`);
+        for (const [, nodeObj] of nodesObj) {
+            if (
+                validate(nodeObj.id) &&
+                (validate(nodeObj.data?.label) || validate(nodeObj.label)) &&
+                (Array.isArray(nodeObj.data?.auxiliaryPorts) || Array.isArray(nodeObj.auxiliaryPorts)) &&
+                (validate(nodeObj.data?.principalPort.id) || validate(nodeObj.principalPort.id))
+            ) {
+                nodes.push({
+                    id: nodeObj.id,
+                    data: nodeObj.data ?? {
+                        label: nodeObj.label,
+                        auxiliaryPorts: nodeObj.auxiliaryPorts,
+                        principalPort: nodeObj.principalPort
+                    },
+                    position: nodeObj.position || {
+                        x: 50 + 300 * Math.floor(index / 5),
+                        y: 50 + 120 * (index % 5)
+                    },
+                    type: 'agent',
+                });
+                index += 1;
+            } else {
+                throw new Error(`Invalid node data structure for id: ${nodeObj.id}`);
+            }
         }
-    });
 
-    return [nodes, edges];
+        const edgesObj = net.edges ? Object.entries(net.edges) : [];
+        const edges: Edge[] = [];
+
+        for (const [, edgeObj] of edgesObj) {
+            if (
+                validate(edgeObj.source) && validate(edgeObj.target) &&
+                (validate(edgeObj.sourcePort) || validate(edgeObj.sourceHandle)) &&
+                (validate(edgeObj.targetPort) || validate(edgeObj.targetHandle))
+            ) {
+                const sourceHandle = edgeObj.sourcePort || edgeObj.sourceHandle;
+                const targetHandle = edgeObj.targetPort || edgeObj.targetHandle;
+                edges.push({
+                    id: edgeObj.id || `E_${edgeObj.source}:${sourceHandle}-${edgeObj.target}:${targetHandle}`,
+                    source: edgeObj.source,
+                    target: edgeObj.target,
+                    sourceHandle: sourceHandle,
+                    targetHandle: `${targetHandle}t`,
+                    animated: edgeObj.activePair ?? edgeObj.animated ?? false,
+                    style: (edgeObj.activePair || edgeObj.animated) ? { stroke: 'blue' } : {},
+                    type: typeEdge,
+                });
+            } else {
+                throw new Error(`Invalid edge data structure for id: ${edgeObj.id}`);
+            }
+        }
+
+        return [nodes, edges];
+    } catch (error) {
+        console.error('Failed to parse JSON:', error);
+        return [[], []];
+    }
 }
